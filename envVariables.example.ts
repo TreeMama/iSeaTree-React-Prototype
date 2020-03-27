62148e5d7bb67c6d7993607cc0d7bb41ace5186d
Kacper Golinski <kacper.golinski@gmail.com>
2020-03-27 20:59:43 +0100

    add firebase package

---
+++ envVariables.example.ts
@@ -0,0 +1,12 @@
+export const envVariables = {
+  OAUTH_ANDROID_CLIENT_ID: '',
+  FIREBASE: {
+    API_KEY: '',
+    AUTH_DOMAIN: '',
+    DATABASE_URL: '',
+    PROJECT_ID: '',
+    STORAGE_BUCKET: '',
+    MESSAGING_SENDER_ID: '',
+    APP_ID: '',
+  },
+}
