e9fb08bdf50ea5eb6f686238257a3573f1f6dae8
Kacper Golinski <kacper.golinski@gmail.com>
2020-03-28 09:45:55 +0100

    add useAuthStateChange hook to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,41 @@
 import React from 'react'
 
 import { registerRootComponent } from 'expo'
-import { Provider as PaperProvider } from 'react-native-paper'
+import { ActivityIndicator, Provider as PaperProvider } from 'react-native-paper'
+import * as firebase from 'firebase'
 
 import { RegisterScreen } from './screens/RegisterScreen'
 import { initializeFirebase } from './config/initializeFirebase'
 
 initializeFirebase()
 
+function useAuthStateChange(): { isAuthLoading: boolean } {
+  const [isAuthLoading, setIsAuthLoading] = React.useState<boolean>(true)
+
+  React.useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      setIsAuthLoading(false)
+
+      if (!!user) {
+        console.log('user is logged:', user)
+      } else {
+        console.log('user is not logged')
+      }
+    })
+  }, [])
+
+  return { isAuthLoading }
+}
+
 export function App() {
+  const { isAuthLoading } = useAuthStateChange()
+
   return (
     <PaperProvider>
-      <RegisterScreen />
+      {/* Instead of ActivityIndicator there could be a splash screen */}
+      <ActivityIndicator animating={isAuthLoading} size="large" style={{ marginTop: 100 }} />
+
+      {!isAuthLoading && <RegisterScreen />}
     </PaperProvider>
   )
 }
