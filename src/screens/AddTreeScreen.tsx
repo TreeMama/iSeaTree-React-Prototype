a0af55117335ff9f8289b3a72bc045c0cb10a93b
Kacper Golinski <kacper.golinski@gmail.com>
2020-03-29 16:00:30 +0200

    update AddTreeScreen

--- src/screens/AddTreeScreen.tsx
+++ src/screens/AddTreeScreen.tsx
@@ -2,11 +2,10 @@ import React from 'react'
 
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 import { StyleSheet, View, Modal, Image, Alert, ScrollView } from 'react-native'
-import { ActivityIndicator, Button, Text } from 'react-native-paper'
+import { Button } from 'react-native-paper'
 import Constants from 'expo-constants'
-import * as Location from 'expo-location'
 
-import { Camera } from '../components/Camera'
+import { CameraWithLocation } from '../components/CameraWithLocation'
 import { colors } from '../styles/theme'
 
 const styles = StyleSheet.create({
@@ -22,7 +21,6 @@ export function AddTreeScreen() {
 
   const [selectedPictureUri, setSelectedPictureUri] = React.useState<string | null>(null)
   const [coords, setCoords] = React.useState<null | { latitude: number; longitude: number }>(null)
-  const [isGettingCoords, setIsGettingCoords] = React.useState<boolean>(false)
 
   function clearAll() {
     setSelectedPictureUri(null)
@@ -39,38 +37,6 @@ export function AddTreeScreen() {
         },
       },
     ])
-  }
-
-  async function getPhotoLocation() {
-    setIsGettingCoords(true)
-    // TODO
-    // if photo exif does not have location data get current location
-    // but only if image was taken just captured!
-
-    try {
-      const { status } = await Location.requestPermissionsAsync()
-      if (status !== Location.PermissionStatus.GRANTED) {
-        // TODO
-        // alert message -> no permission for location
-        return
-      }
-
-      const location = await Location.getCurrentPositionAsync({
-        accuracy: Location.Accuracy.BestForNavigation,
-      })
-
-      console.log('location: ', location)
-
-      setCoords({ longitude: location.coords.longitude, latitude: location.coords.latitude })
-      setIsGettingCoords(false)
-    } catch (error) {
-      if (__DEV__) {
-        console.log(error)
-      }
-
-      setIsGettingCoords(false)
-      // TODO: handle possible errors
-    }
   }
 
   const canClear: boolean = !!selectedPictureUri
@@ -94,19 +60,12 @@ export function AddTreeScreen() {
           alignItems: 'center',
         }}
       >
-        {/* TODO: maybe refactor below condition  */}
-        {!!selectedPictureUri &&
-          (isGettingCoords ? (
-            <>
-              <ActivityIndicator animating={true} size="large" color={colors.gray[400]} />
-              <Text style={{ color: colors.gray[400], marginTop: 15 }}>Getting coordinates</Text>
-            </>
-          ) : (
-            <Image
-              style={{ height: '100%', width: '100%', resizeMode: 'contain' }}
-              source={{ uri: selectedPictureUri }}
-            />
-          ))}
+        {!!selectedPictureUri && (
+          <Image
+            style={{ height: '100%', width: '100%', resizeMode: 'contain' }}
+            source={{ uri: selectedPictureUri }}
+          />
+        )}
 
         {!selectedPictureUri && (
           <MaterialCommunityIcons name="image" size={60} color={colors.gray[400]} />
@@ -123,14 +82,13 @@ export function AddTreeScreen() {
       </Button>
 
       <Modal visible={isCameraVisible} animationType="slide">
-        <Camera
+        <CameraWithLocation
           onClose={() => {
             setIsCameraVisible(false)
           }}
-          onTakePicture={(capturedPicture) => {
+          onTakePictureFinish={({ capturedPicture }) => {
             setSelectedPictureUri(capturedPicture.uri)
             setIsCameraVisible(false)
-            getPhotoLocation()
           }}
         />
       </Modal>
