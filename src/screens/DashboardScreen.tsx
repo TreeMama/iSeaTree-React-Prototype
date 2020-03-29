4ea0bbc0513d4a2141fd99ae9755f9652ded6bfc
Kacper Golinski <kacper.golinski@gmail.com>
2020-03-29 16:03:27 +0200

    Update DashboardScreen.tsx

--- src/screens/DashboardScreen.tsx
+++ src/screens/DashboardScreen.tsx
@@ -10,14 +10,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     paddingTop: 50,
+    padding: 20,
   },
 })
 
 export function DashboardScreen() {
   return (
     <View style={styles.container}>
-      <Title>Dashboard</Title>
-      <Paragraph>do something here</Paragraph>
+      <Title>TODO: Dashboard</Title>
 
       <Button style={{ marginTop: 150 }} onPress={signOutUser}>
         Sign out
