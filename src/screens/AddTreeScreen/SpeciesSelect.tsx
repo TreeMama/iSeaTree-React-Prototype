5acddf91208e1f66dd126bce16628bcc281213c4
Kacper Golinski <kacper.golinski@gmail.com>
2020-04-06 14:15:30 +0200

    Update SpeciesSelect.tsx

--- src/screens/AddTreeScreen/SpeciesSelect.tsx
+++ src/screens/AddTreeScreen/SpeciesSelect.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 
 import { Modal, View, FlatList, TouchableOpacity } from 'react-native'
 import { Subheading, useTheme, Button, TextInput, List } from 'react-native-paper'
-import RNPickerSelect from 'react-native-picker-select'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 import { colors } from '../../styles/theme'
@@ -20,28 +19,9 @@ interface SpeciesSelectProps {
   onSelect: (speciesData: null | SpeciesData) => void
 }
 
-const labels: { [key in Species]: string } = {
-  COMMON: 'Common',
-  SCIENTIFIC: 'Scientific',
-}
-
-// function getSpeciesItems(speciesType: Species): { label: string; value: number }[] {
-//   return speciesData.map((speciesDatum) => ({
-//     label: speciesDatum[speciesType],
-//     value: speciesDatum.ID,
-//   }))
-// }
-
 function getSpeciesFlatListData(
   query?: string,
 ): { ID: string; COMMON: string; SCIENTIFIC: string }[] {
-  // const full = speciesData.map((speciesDatum) => ({
-  //   title: speciesDatum[speciesType],
-  //   id: speciesDatum.ID,
-  // }))
-
-  console.log('getSpeciesFlatListData....')
-
   if (!query) {
     return speciesDataList
   }
@@ -49,7 +29,7 @@ function getSpeciesFlatListData(
   const inputValue = query.trim().toLowerCase()
   const inputLength = inputValue.length
 
-  if (inputLength === 0) {
+  if (inputLength < 3) {
     return speciesDataList
   }
 
@@ -69,27 +49,12 @@ export function SpeciesSelect(props: SpeciesSelectProps) {
   const [isModalVisible, setIsModalVisible] = React.useState<boolean>(false)
   const theme = useTheme()
 
-  // const currentSpeciesType = props.speciesType
-  const currentSpeciesNamesItems = getSpeciesFlatListData(query)
-
-  // function handleSpeciesTypeChange(speciesType: Species) {
-  //   if (currentSpeciesType === speciesType) {
-  //     return
-  //   }
-
-  //   props.onSelect({ speciesNameId: props.speciesNameId, speciesType: speciesType })
-  // }
-
-  // function handleSpeciesNameChange(speciesNameId: null | number) {
-  //   if (!speciesNameId) {
-  //     return
-  //   }
-
-  //   props.onSelect({ speciesNameId, speciesType: currentSpeciesType })
-  // }
+  const currentSpeciesNamesItems = React.useMemo(() => getSpeciesFlatListData(query), [query])
 
   function handleSpeciesSelect(speciesData: SpeciesData) {
-    props.onSelect(speciesData)
+    setTimeout(() => {
+      props.onSelect(speciesData)
+    }, 100)
   }
 
   function getItemTitleStyle(speciesDataItem: SpeciesData) {
@@ -106,51 +71,9 @@ export function SpeciesSelect(props: SpeciesSelectProps) {
 
       <View
         style={{
-          // borderWidth: 1,
-          // borderRadius: theme.roundness,
-          // backgroundColor: theme.colors.background,
-          // borderColor: theme.colors.placeholder,
           marginTop: 5,
         }}
       >
-        {/* <View
-          style={{
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'space-between',
-            padding: 10,
-            paddingLeft: 15,
-          }}
-        >
-          <Button
-            uppercase={false}
-            mode={currentSpeciesType === Species.COMMON ? 'outlined' : 'text'}
-            style={{ marginRight: 10 }}
-            onPress={() => {
-              handleSpeciesTypeChange(Species.COMMON)
-            }}
-          >
-            {labels.COMMON}
-          </Button>
-          <Button
-            uppercase={false}
-            mode={currentSpeciesType === Species.SCIENTIFIC ? 'outlined' : 'text'}
-            onPress={() => {
-              handleSpeciesTypeChange(Species.SCIENTIFIC)
-            }}
-          >
-            {labels.SCIENTIFIC}
-          </Button>
-        </View> */}
-
-        {/* <Button
-          onPress={() => {
-            setIsModalVisible(true)
-          }}
-        >
-          Select species...
-        </Button> */}
-
         <TouchableOpacity
           onPress={() => {
             setIsModalVisible(true)
@@ -158,13 +81,17 @@ export function SpeciesSelect(props: SpeciesSelectProps) {
           activeOpacity={0.3}
         >
           <View pointerEvents="box-only">
-            <TextInput editable={false} mode="outlined" placeholder="Select species..." />
+            <TextInput
+              editable={false}
+              mode="outlined"
+              placeholder="Select species..."
+              value={props.speciesData?.COMMON}
+            />
             <View
               style={{
                 position: 'absolute',
                 right: 0,
                 height: '100%',
-                // borderWidth: 1,
                 justifyContent: 'center',
                 alignItems: 'center',
                 paddingRight: 15,
@@ -216,41 +143,6 @@ export function SpeciesSelect(props: SpeciesSelectProps) {
             />
           </View>
         </Modal>
-
-        {/* <RNPickerSelect
-          value={props.speciesNameId}
-          onValueChange={handleSpeciesNameChange}
-          placeholder={{
-            label: 'Select species name...',
-            value: null,
-          }}
-          useNativeAndroidPickerStyle={false}
-          style={{
-            modalViewMiddle: { justifyContent: 'flex-end' },
-            modalViewBottom: { backgroundColor: '#fff' },
-            chevronContainer: { display: 'none' },
-            done: { color: theme.colors.primary },
-            iconContainer: {
-              height: '100%',
-              justifyContent: 'center',
-              alignItems: 'center',
-              paddingRight: 15,
-            },
-          }}
-          textInputProps={{
-            height: 50,
-            borderTopWidth: 1,
-            borderColor: '#ddd',
-            paddingHorizontal: 15,
-            fontSize: 15,
-            borderRadius: theme.roundness,
-          }}
-          Icon={() => (
-            <MaterialCommunityIcons name="menu-down" size={30} color={colors.gray[500]} />
-          )}
-          placeholderTextColor={theme.colors.backdrop}
-          items={currentSpeciesNamesItems}
-        /> */}
       </View>
     </View>
   )
