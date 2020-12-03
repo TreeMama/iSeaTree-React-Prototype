be7c75eb42839f0ebcb3890e63dfa67f717bf6cc
Hal Mueller <halmueller@gmail.com>
2020-12-02 20:15:07 -0800

    update for the non-Slack version of envVariables.ts, and remove -p from shasum invocation

--- verify-checksums.sh
+++ verify-checksums.sh
@@ -9,10 +9,10 @@ then
 fi
 
 # After changing envVariables.ts, recreate the checksum file with this command:
-#     shasum -p -a 512 envVariables.ts > checksums.txt
+#     shasum -a 512 envVariables.ts > checksums.txt
 # Commit checksums.txt to git. Do not commit envVariables.ts.
 
-if ! shasum -s -p -a 512 --check checksums.txt; then
+if ! shasum -s -a 512 --check checksums.txt; then
     echo "Your copy of $CONFIG_FILE is out of date. Please obtain the current version from a project administrator."
     exit 1
 fi
