b5ee605653cf7f1c91f8d715b7e908f479b3ca2b
Ryan Flynn <ryanflynn44@gmail.com>
2021-02-02 16:38:57 -0500

    Added the images and badge logic for 20, 50, 100, 200

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { Image, View, ImageSourcePropType } from 'react-native'
 
-type BadgeVariant = 'seedling' | 'sapling' | 'old_growth_expert' | 'first_tree' | 'fifth_tree' | 'tenth_tree' | 'dbh'
+type BadgeVariant = 'seedling' | 'sapling' | 'old_growth_expert' | 'first_tree' | 'fifth_tree' | 'tenth_tree' | 'twentieth_tree' | 'fiftieth_tree' | 'hundredth_tree' | 'two_hundredth_tree' | 'dbh'
 
 interface BadgeProps {
   key: string
@@ -16,6 +16,10 @@ const badgeImages: { [key in BadgeVariant]: ImageSourcePropType } = {
   first_tree: require('../../assets/badges/first_tree.png'),
   fifth_tree: require('../../assets/badges/fifth_tree.png'),
   tenth_tree: require('../../assets/badges/tenth_tree.png'),
+  twentieth_tree: require('../../assets/badges/twentieth_tree.png'),
+  fiftieth_tree: require('../../assets/badges/fiftieth_tree.png'),
+  hundredth_tree: require('../../assets/badges/hundredth_tree.png'),
+  two_hundredth_tree: require('../../assets/badges/two_hundredth_tree.png'),
   dbh: require('../../assets/badges/dbh.png')
 }
 
@@ -28,11 +32,13 @@ export function Badge(props: BadgeProps) {
     <View>
       <Image
         source={badgeImage}
-        style={{ height: BADGE_HEIGHT,
-                 width: BADGE_HEIGHT,
-                 opacity: 0.8,
-                 resizeMode: 'stretch',
-                 margin: 10 }}
+        style={{
+          height: BADGE_HEIGHT,
+          width: BADGE_HEIGHT,
+          opacity: 0.8,
+          resizeMode: 'stretch',
+          margin: 10
+        }}
       />
     </View>
   )
