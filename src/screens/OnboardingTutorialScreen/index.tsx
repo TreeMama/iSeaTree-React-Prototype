c47b0072585a65ae636afa5dfc3453d40945c136
Ryan Flynn <ryanflynn44@gmail.com>
2021-04-26 16:42:21 -0400

    Onboarding Screen Skeleton

---
+++ src/screens/OnboardingTutorialScreen/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text, Button, StyleSheet} from 'react-native';
+
+const OnboardingScreen = () => {
+    return(
+        <View>
+            <Text>Onboarding Screen</Text>
+        </View>
+            
+    );
+};
