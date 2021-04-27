e99a07135fe260547612d2b341fdbca203b5fb5a
Ryan Flynn <ryanflynn44@gmail.com>
2021-04-27 11:01:19 -0400

    Added first onboarding image, and the onboarding dependency

--- src/screens/OnboardingTutorialScreen/index.tsx
+++ src/screens/OnboardingTutorialScreen/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {View, Text, Button, StyleSheet} from 'react-native';
+import Onboarding from 'react-native-onboarding-swiper';
 
 const OnboardingScreen = () => {
     return(
