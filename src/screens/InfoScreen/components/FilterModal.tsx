92e91bef186eb1c0e42457daf44da2d985308ab7
dddwj <dwj2930@163.com>
2022-11-23 18:30:40 -0800

    TreeInfo Filter UI Change

--- src/screens/InfoScreen/components/FilterModal.tsx
+++ src/screens/InfoScreen/components/FilterModal.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react"
-import { Modal, View } from "react-native"
+import { Modal, View, ScrollView, Image } from "react-native"
 import { TouchableOpacity } from "react-native-gesture-handler"
 import { Text } from 'react-native-paper'
 import { styles } from "../styles"
 import { IFilterValues } from "../types"
+const returnButton = require('../../../../assets/angle-left.png')
 
 /**
  * The Button Tab at the top of Info screen containing Genus | Species.
@@ -22,89 +23,104 @@ export const FilterModal = (props: {
     <View style={{ backgroundColor: 'rgba(0,0,0,0.5)', height: '30%' }}>
     </View>
     <View style={{ backgroundColor: 'white', height: '70%' }}>
-      <TouchableOpacity onPress={() => props.setShowFilters(false)}>
-        <Text>{'return button'}</Text>
+      <TouchableOpacity
+        onPress={() => props.setShowFilters(false)}
+      >
+        <Image style={{ maxHeight: '60%', resizeMode: 'contain' }} source={returnButton}></Image>
       </TouchableOpacity>
       <View style={{ alignItems: 'center' }}>
-        <Text >{'Filters'}</Text>
+        <Text style={{ fontSize: 17, margin: 24 }} >{'Filters'}</Text>
       </View>
-      <Text >{'Name type'}</Text>
-      <FilterButton
-        isActive={props.filterValues.allNameTypes}
-        buttonText="All"
-        toggleOnHandler={() => {
-          props.setFilterValues({ ...props.filterValues, allNameTypes: true, commonName: false, scientificName: false })
-        }}
-      />
-      <FilterButton
-        isActive={props.filterValues.commonName}
-        buttonText="Common Name"
-        toggleOnHandler={() => {
-          props.setFilterValues({ ...props.filterValues, allNameTypes: false, commonName: true, scientificName: false })
-        }}
-      />
-      <FilterButton
-        isActive={props.filterValues.scientificName}
-        buttonText="Scientific Name"
-        toggleOnHandler={() => {
-          props.setFilterValues({ ...props.filterValues, allNameTypes: false, commonName: false, scientificName: true })
-        }}
-      />
 
-      <Text>{'Tree type'}</Text>
-      <FilterButton
-        isActive={props.filterValues.allTreeTypes}
-        buttonText="All"
-        toggleOnHandler={() => {
-          props.setFilterValues({ ...props.filterValues, allTreeTypes: true, conifer: false, broadleaf: false })
-        }}
-      />
-      <FilterButton
-        isActive={props.filterValues.conifer}
-        buttonText="Conifer"
-        toggleOnHandler={() => {
-          props.setFilterValues({ ...props.filterValues, allTreeTypes: false, conifer: true, broadleaf: false })
-        }}
-      />
-      <FilterButton
-        isActive={props.filterValues.broadleaf}
-        buttonText="Broadleaf"
-        toggleOnHandler={() => {
-          props.setFilterValues({ ...props.filterValues, allTreeTypes: false, conifer: false, broadleaf: true })
-        }}
-      />
+      <Text style={{ fontSize: 15, marginLeft: 10 }} >{'Name type'}</Text>
+      <View style={{ width: '100%' }}>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          <FilterButton
+            isActive={props.filterValues.allNameTypes}
+            buttonText="All"
+            toggleOnHandler={() => {
+              props.setFilterValues({ ...props.filterValues, allNameTypes: true, commonName: false, scientificName: false })
+            }}
+          />
+          <FilterButton
+            isActive={props.filterValues.commonName}
+            buttonText="Common Name"
+            toggleOnHandler={() => {
+              props.setFilterValues({ ...props.filterValues, allNameTypes: false, commonName: true, scientificName: false })
+            }}
+          />
+          <FilterButton
+            isActive={props.filterValues.scientificName}
+            buttonText="Scientific Name"
+            toggleOnHandler={() => {
+              props.setFilterValues({ ...props.filterValues, allNameTypes: false, commonName: false, scientificName: true })
+            }}
+          />
+        </ScrollView>
+      </View>
 
-      <Text>{'Level of Difficulty'}</Text>
-      <FilterButton
-        isActive={props.filterValues.allDifficulties}
-        buttonText="All"
-        toggleOnHandler={() => {
-          props.setFilterValues({ ...props.filterValues, allTreeTypes: true, conifer: false, broadleaf: false })
-        }}
-      />
-      <FilterButton
-        isActive={props.filterValues.easy}
-        buttonText="Easy"
-        toggleOnHandler={() => {
-          props.setFilterValues({ ...props.filterValues, allTreeTypes: false, conifer: true, broadleaf: false })
-        }}
-      />
-      <FilterButton
-        isActive={props.filterValues.medium}
-        buttonText="Medium"
-        toggleOnHandler={() => {
-          props.setFilterValues({ ...props.filterValues, allTreeTypes: false, conifer: false, broadleaf: true })
-        }}
-      />
-      <FilterButton
-        isActive={props.filterValues.expert}
-        buttonText="Expert"
-        toggleOnHandler={() => {
-          props.setFilterValues({ ...props.filterValues, allTreeTypes: false, conifer: false, broadleaf: true })
-        }}
-      />
+      <Text style={{ fontSize: 15, marginLeft: 10 }}>{'Tree type'}</Text>
+      <View style={{ width: '100%' }}>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          <FilterButton
+            isActive={props.filterValues.allTreeTypes}
+            buttonText="All"
+            toggleOnHandler={() => {
+              props.setFilterValues({ ...props.filterValues, allTreeTypes: true, conifer: false, broadleaf: false })
+            }}
+          />
+          <FilterButton
+            isActive={props.filterValues.conifer}
+            buttonText="Conifer"
+            toggleOnHandler={() => {
+              props.setFilterValues({ ...props.filterValues, allTreeTypes: false, conifer: true, broadleaf: false })
+            }}
+          />
+          <FilterButton
+            isActive={props.filterValues.broadleaf}
+            buttonText="Broadleaf"
+            toggleOnHandler={() => {
+              props.setFilterValues({ ...props.filterValues, allTreeTypes: false, conifer: false, broadleaf: true })
+            }}
+          />
+        </ScrollView>
+      </View>
+
+      <Text style={{ fontSize: 15, marginLeft: 10 }}>{'Level of Difficulty'}</Text>
+      <View style={{ width: '100%' }}>
+        <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+          <FilterButton
+            isActive={props.filterValues.allDifficulties}
+            buttonText="All"
+            toggleOnHandler={() => {
+              props.setFilterValues({ ...props.filterValues, allTreeTypes: true, conifer: false, broadleaf: false })
+            }}
+          />
+          <FilterButton
+            isActive={props.filterValues.easy}
+            buttonText="Easy"
+            toggleOnHandler={() => {
+              props.setFilterValues({ ...props.filterValues, allTreeTypes: false, conifer: true, broadleaf: false })
+            }}
+          />
+          <FilterButton
+            isActive={props.filterValues.medium}
+            buttonText="Medium"
+            toggleOnHandler={() => {
+              props.setFilterValues({ ...props.filterValues, allTreeTypes: false, conifer: false, broadleaf: true })
+            }}
+          />
+          <FilterButton
+            isActive={props.filterValues.expert}
+            buttonText="Expert"
+            toggleOnHandler={() => {
+              props.setFilterValues({ ...props.filterValues, allTreeTypes: false, conifer: false, broadleaf: true })
+            }}
+          />
+        </ScrollView>
+      </View>
     </View>
-  </Modal>
+  </Modal >
 }
 
 // generalizes the appearance of a filter button
@@ -120,7 +136,7 @@ const FilterButton = (props: {
   const inactiveTextColor = '#62717A'
   return <TouchableOpacity
     style={{
-      height: 50, maxWidth: '20%', borderWidth: 2, borderRadius: 10, margin: 10,
+      height: 50, minWidth: '20%', borderWidth: 2, borderRadius: 10, margin: 10, padding: 10,
       alignItems: 'center',
       justifyContent: 'center',
       backgroundColor: props.isActive ? activeBackgroundColor : inactiveBackgroundColor
