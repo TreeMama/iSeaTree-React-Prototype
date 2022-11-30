17e9815726fe1e5fc79fc24ab5f51dc60629cd5c
bestw07 <95283860+bestw07@users.noreply.github.com>
2022-12-01 06:11:03 +0700

    v 3+ (BUG) Explore menu - the 'single user' icon is overlapping the new Nav menu #333

--- src/screens/MapScreen.tsx
+++ src/screens/MapScreen.tsx
@@ -847,7 +847,7 @@ export function MapScreen(props: { navigation: MapScreenNavigation }) {
         onPress={() => setOwnmap()}
         style={[
           styles.touchableOpacityStyle,
-          { backgroundColor: isActiveown ? colors.gray[600] : '#fff' },
+          { backgroundColor: isActiveown ? colors.gray[600] : '#fff', bottom: '12%' },
         ]}
       >
         <MaterialCommunityIcons
@@ -861,7 +861,7 @@ export function MapScreen(props: { navigation: MapScreenNavigation }) {
         onPress={() => setPublicmap()}
         style={[
           styles.touchableOpacityStyle,
-          { bottom: 105, backgroundColor: !isActiveown ? colors.gray[600] : '#fff' },
+          { bottom: '22%', backgroundColor: !isActiveown ? colors.gray[600] : '#fff' },
         ]}
       >
         <Image
