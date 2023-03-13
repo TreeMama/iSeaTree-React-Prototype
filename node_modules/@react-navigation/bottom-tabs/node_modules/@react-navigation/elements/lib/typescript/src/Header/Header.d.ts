42c98489200b6ce379154079c0139c4a9959f243
Stephen Clemmer <stephenjclemmer@gmail.com>
2023-03-12 17:19:34 -0700

    the 'stephen' branch was able to be opened in XCode by Stephen Clemmer

---
+++ node_modules/@react-navigation/bottom-tabs/node_modules/@react-navigation/elements/lib/typescript/src/Header/Header.d.ts
@@ -0,0 +1,19 @@
+/// <reference types="react" />
+import type { HeaderOptions, Layout } from '../types';
+declare type Props = HeaderOptions & {
+    /**
+     * Whether the header is in a modal
+     */
+    modal?: boolean;
+    /**
+     * Layout of the screen.
+     */
+    layout?: Layout;
+    /**
+     * Title text for the header.
+     */
+    title: string;
+};
+export default function Header(props: Props): JSX.Element;
+export {};
+//# sourceMappingURL=Header.d.ts.map
