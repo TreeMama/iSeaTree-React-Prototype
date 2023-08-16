220163c2b933c194fae3047c94fe097a9e0ea460
bestw07 <devspeed.vision.it@gmail.com>
2023-08-16 05:44:43 +0200

    fix android status range issue

--- src/screens/AddTreeScreen/lib/uploadTreeImage.tsx
+++ src/screens/AddTreeScreen/lib/uploadTreeImage.tsx
@@ -3,8 +3,8 @@ import { v4 as uuidV4 } from 'uuid'
 import { uploadImage, firebaseImagePath, ImageDownloadUrl } from '../../../lib/firebaseServices'
 
 export async function uploadTreeImage(imageUri: string): Promise<ImageDownloadUrl> {
-  const response = await fetch(imageUri)
-  const blob = await response.blob()
+  // const response = await fetch(imageUri)
+  // const blob = await response.blob()
 
   const fileName: string = uuidV4()
   const firebaseStoragePath = firebaseImagePath.trees(`${fileName}.jpg`)
